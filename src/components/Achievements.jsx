0db98f81ef428857db96901a7bb533595081c40a
Obayaa <doraappiah2004@gmail.com>
2025-06-29 18:11:42 +0000

    feat: add Achievements and Card components for displaying project statistics

---
+++ src/components/Achievements.jsx
@@ -0,0 +1,18 @@
+import { Card } from "./Card"
+import Section_Title from "./Section_Title"
+
+export default function Achievements() {
+    return (
+        <div className="max-w-7xl mx-auto px-8 flex flex-col gap-14">
+            <Section_Title title={"Our Achievements"} />
+            <div className="flex gap-6 justify-between">
+                <Card value={"150+"} title={"Projects Completed"} />
+                <Card value={"50+"} title={"Modern Homes"} />
+                <Card value={"75+"} title={"Happy Clients"} />
+                <Card value={"25+"} title={"Years Experience"} />
+
+            </div>
+
+        </div>
+    )
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+export function Card({ value, title }) {
+    return (
+        <div className="bg-red-600 size-48 p-10 rounded-md text-white text-center flex flex-col justify-center gap-4 shadow-md hover:scale-105 duration-300">
+            <h1 className="text-5xl font-black">{value}</h1>
+            <p className="text-xl">{title}</p>
+        </div>
+    );
+}
