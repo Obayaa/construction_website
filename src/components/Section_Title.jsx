98871d637a2219cce5ba8b91fee595cbb8f78dbe
Obayaa <doraappiah2004@gmail.com>
2025-06-29 18:11:48 +0000

    feat: add Section_Title component for displaying section titles

---
+++ src/components/Section_Title.jsx
@@ -0,0 +1,8 @@
+export default function Section_Title({ title }) {
+    return (
+        <h1
+            className='text-5xl text-center font-bold text-blue-800 underline underline-offset-[16px] decoration-red-600'>
+            {title}
+        </h1>
+    )
+}
