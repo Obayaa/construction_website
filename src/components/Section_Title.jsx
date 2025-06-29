af669e64f17b33538d6a9cfd2e49c2f979e89812
Obayaa <doraappiah2004@gmail.com>
2025-06-29 21:15:28 +0000

    feat: enhance Section_Title component styling for improved responsiveness

--- src/components/Section_Title.jsx
+++ src/components/Section_Title.jsx
@@ -1,7 +1,7 @@
 export default function Section_Title({ title }) {
     return (
         <h1
-            className='text-5xl text-center font-bold text-blue-800 underline underline-offset-[16px] decoration-red-600'>
+            className='lg:text-5xl xs:2xl text-4xl text-center font-bold text-blue-800 underline underline-offset-[16px] decoration-red-600'>
             {title}
         </h1>
     )
