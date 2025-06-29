724ebac7c128b44a4d5f5e7c8b70d3b7ae7429cf
Obayaa <doraappiah2004@gmail.com>
2025-06-29 18:12:07 +0000

    feat: update App component layout and add sections for Home, About, Achievements, Services, Projects, Team, and Contact

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,37 @@ import Team from './components/Team';
 function App() {
   return (
     <div className="min-h-screen">
-      
+      <Navbar />
+      <div className="">
+        <section id="home" className="">
+          <Home />
+        </section>
+
+        <section id="about" className="min-h-screen pt-20 bg-white">
+          <About />
+        </section>
+
+        <section id="achievements" className="min-h-96 p-10 bg-blue-100">
+          <Achievements />
+        </section>
+
+        <section id="services" className="min-h-screen pt-20 p-10 bg-gray-100 text-4xl flex">
+          <div>Services Section</div>
+        </section>
+
+        <section id="projects" className="min-h-screen pt-20 p-10 bg-white text-4xl flex">
+          <div>Hello everyone, welcome to projects done</div>
+        </section>
+
+        <section id="team" className="min-h-screen p-10 bg-gray-100">
+          <Team />
+
+        </section>
+        <section id="contact" className="min-h-screen pt-20 p-10 bg-white text-4xl flex">
+          <div>Contact Section</div>
+        </section>
+
+      </div>
     </div>
   );
 }
