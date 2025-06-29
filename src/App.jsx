c07f7df383988d60cdb3b027781b289b8c8707f3
Obayaa <doraappiah2004@gmail.com>
2025-06-29 18:10:01 +0000

    feat: initialize React project with Vite and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App.css to import Tailwind CSS styles.
    - Developed App.jsx component structure
    - Established index.css with base styles and responsive design considerations.
    - Set up main.jsx for rendering the App component within StrictMode.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import About from './components/About';
+import Achievements from './components/Achievements';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Team from './components/Team';
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      
+    </div>
+  );
+}
+
+export default App;
