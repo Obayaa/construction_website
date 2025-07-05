528f6405daf1aa7fcd502a671141b8beee2d8564
Obayaa <doraappiah2004@gmail.com>
2025-07-05 23:00:18 +0000

    feat: enhance Home component with responsive design and animations; update Section_Title styles for consistency

--- src/components/Section_Title.jsx
+++ src/components/Section_Title.jsx
@@ -1,7 +1,7 @@
 export default function Section_Title({ title }) {
     return (
         <h1
-            className='lg:text-5xl xs:2xl text-4xl text-center font-bold text-blue-800 underline underline-offset-[16px] decoration-red-600'>
+            className='lg:text-5xl text-3xl md:text-4xl text-center font-bold text-blue-800 border-0 border-b-4 border-b-red-600 pb-3 '>
             {title}
         </h1>
     )
