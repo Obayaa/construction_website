bc52e37c034c7698b57bc9a35ca676e9f7f9a2ac
Obayaa <doraappiah2004@gmail.com>
2025-07-05 23:00:00 +0000

    feat: restructure Contact component layout for improved readability and organization

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,24 @@
 import { Mail } from "lucide-react";
 import Section_Title from "./Section_Title";
+// import { flyer } from '../assets/flyer.jpeg'
 
 export default function Contact() {
     return (
 
         <div className="max-w-5xl mx-auto">
             <Section_Title title={"Contact Us"} />
-            <div className="">
-                <p>Get in Touch</p>
-                <div>
-                    <Mail width={24} height={24} className=" bg-blue-100 rounded-full" />
+            <div>
+                <div className="bg-white shadow-md size-fit p-4 rounded-lg ">
+                    <p>Get in Touch</p>
                     <div>
-                        <h1>Locations</h1>
-                        <p>Accra, Sunyani, Kumasi, Ghana.</p>
+                        <Mail size={24} className="bg-blue-100 rounded-full" />
+                        <div>
+                            <h1>Locations</h1>
+                            <p>Accra, Sunyani, Kumasi, Ghana.</p>
+                        </div>
                     </div>
                 </div>
+                {/* <img src={flyer} alt="" /> */}
             </div>
         </div>
     )
