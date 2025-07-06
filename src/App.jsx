7ff6cbf594c69680ecca0f88ab8ad98c3139de10
Obayaa <doraappiah2004@gmail.com>
2025-07-06 03:13:13 +0000

    style: update section styles in App component for improved layout and consistency

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,34 @@ import Team from './components/Team';
 function App() {
   return (
     <div className="min-h-screen">
-      
+
       <Navbar />
       <div className="">
-        <section id="home" className="">
+        <section id="home" className="scroll-mt-56 md:scroll-mt-0">
           <Home />
         </section>
 
-        <section id="about" className="min-h-screen pt-20 bg-white">
+        <section id="about" className="min-h-screen pt-12 pb-8 bg-white scroll-mt-64 md:scroll-mt-0">
           <About />
         </section>
 
-        <section id="achievements" className="min-h-[27rem] p-10 lg:p-10 bg-blue-100">
+        <section id="achievements" className="min-h-[27rem] py-10 bg-blue-100">
           <Achievements />
         </section>
 
-        <section id="services" className="min-h-screen pt-20 p-10 bg-gray-100 text-4xl flex">
+        <section id="services" className="min-h-screen pt-20 p-10 bg-gray-100 text-4xl flex scroll-mt-56 md:scroll-mt-0">
           <div>Services Section</div>
         </section>
 
-        <section id="projects" className="min-h-screen pt-20 p-10 bg-white text-4xl flex">
+        <section id="projects" className="min-h-screen pt-20 p-10 bg-white text-4xl flex scroll-mt-56 md:scroll-mt-0">
           <div>Hello everyone, welcome to projects done</div>
         </section>
 
-        <section id="team" className="min-h-[70vh] lg:p-10 pt-20 bg-blue-50">
+        <section id="team" className="min-h-[70vh] py-20 bg-blue-50 scroll-mt-56 md:scroll-mt-0">
           <Team />
 
         </section>
-        <section id="contact" className="min-h-screen pt-20 p-10 bg-white">
+        <section id="contact" className="min-h-screen pt-20 bg-gray-50">
           <Contact />
         </section>
 
