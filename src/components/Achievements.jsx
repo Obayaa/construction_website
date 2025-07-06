f2297789b4fb7b5fa099d61a2377739413dc56ab
Obayaa <doraappiah2004@gmail.com>
2025-07-06 05:35:45 +0000

    extracted achievementsData to codes more readable

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -2,15 +2,22 @@ import { Card } from "./Card"
 import Section_Title from "./Section_Title"
 
 export default function Achievements() {
+    const achievementsData = [
+        { value: "150+", title: "Projects Completed" },
+        { value: "50+", title: "Modern Homes" },
+        { value: "75+", title: "Happy Clients" },
+        { value: "25+", title: "Years Experience" },
+    ];
+
     return (
         <div className="max-w-7xl mx-auto px-4 lg:px-8 flex flex-col">
             <Section_Title title={"Our Achievements"} />
-            <div className="flex gap-6 lg:justify-between flex-wrap p-3 justify-center lg:flex-nowrap">
-                <Card value={"150+"} title={"Projects Completed"} />
-                <Card value={"50+"} title={"Modern Homes"} />
-                <Card value={"75+"} title={"Happy Clients"} />
-                <Card value={"25+"} title={"Years Experience"} />
+
+            <div className="flex gap-6 lg:justify-between flex-wrap p-3 justify-center lg:flex-nowrap mt-12">
+                {achievementsData.map((achievement, index) => (
+                    <Card key={index} {...achievement} />
+                ))}
             </div>
         </div>
-    )
+    );
 }
