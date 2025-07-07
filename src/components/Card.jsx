be73d5d4f9ef4c576d2a8bc496a0016485755e01
Obayaa <doraappiah2004@gmail.com>
2025-07-07 08:30:15 +0000

    Deleted commented codes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,23 +18,6 @@ export function Card({ value, title }) {
     const endValue = parseInt(value.replace('+', ''), 10);
     const suffix = value.includes('+') ? '+' : '';
 
-    // if (isMobile) {
-    //     return (
-    //         <div className="bg-red-600 w-sm h-36 lg:size-48 p-10 rounded-md text-white text-center flex flex-col justify-center gap-4 shadow-lg hover:scale-105 duration-300">
-    //             <h1 className="lg:text-5xl text-4xl font-black">{value}</h1>
-    //             <p className="text-xl">{title}</p>
-    //         </div>
-    //     );
-    // }
-    // return (
-    //     <Slide direction="left" duration={1000} cascade>
-    //         <div className="bg-red-600 size-48 p-10 rounded-md text-white text-center flex flex-col justify-center gap-4 shadow-lg hover:scale-105 duration-300">
-    //             <h1 className="lg:text-5xl text-3xl font-black">{value}</h1>
-    //             <p className="text-xl">{title}</p>
-    //         </div>
-    //     </Slide>
-    // );
-
     return (
         <div ref={ref} className="bg-blue-600 text-white text-center flex flex-col justify-center gap-2 shadow-lg rounded-lg p-6 w-full sm:w-48 h-40 transform transition-transform duration-300 hover:scale-105 hover:-translate-y-2">
             <h1 className="text-5xl font-black">
